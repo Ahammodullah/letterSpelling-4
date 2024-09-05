bf831f7996360bcd50aebd9486bca6716e094b94
Ahammod <ahammodullah12@gmail.com>
2024-09-05 22:03:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+ //letter spelling
+//Vowel
+
+
+
+var letter=prompt("Enter any letter : ");
+
+
+switch(letter){
+    case "a":
+        document.write("Vowel");
+        break
+
+        case "e":
+        document.write("Vowel");
+        break
+
+        case "i":
+        document.write("Vowel");
+        break
+
+        case "o":
+        document.write("Vowel");
+        break
+
+        case "u":
+        document.write("Vowel");
+        break
+
+        default:
+        document.write("Consonant");
+}
